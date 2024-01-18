dcf8583fc8dc3c281ce7396efa780ae41b7e2e8a
thatimoreira <thatimoreira.dev@gmail.com>
2024-01-17 21:40:41 -0300

    Atualização da mensagem de parabenização pelo acerto do número secreto agora exibe o valor do número

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,9 +1,13 @@
 alert('Boas vindas ao Jogo do Número Secreto!');
 
-let numeroSecreto = 29;
+let numeroSecreto = 11;
 let chute = prompt('Escolha um número entre 1 e 30: ');
 
 if (chute == numeroSecreto)
 {
-    console.log('Parabéns! Você acertou o número secreto!');
+    console.log(`Parabéns! Você acertou o número secreto: ${numeroSecreto}!`);
+}
+else
+{
+    alert('Tente outra vez!  :(');
 }
