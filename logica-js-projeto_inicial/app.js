0d240259d743fc75343ab0b6341f35e1a00ff686
thatimoreira <thatimoreira.dev@gmail.com>
2024-01-18 00:05:30 -0300

    Implementação de lógica que dá dicas ao usuário comparando sua escolha ao número secreto e informando se ele émaior ou menor ao número que deve acertar e foi implementada também condição que limita a quantidade de tentativas a no máximo três palpites

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,21 +1,31 @@
 alert('Boas vindas ao Jogo do Número Secreto!');
 
 let numeroSecreto = 11;
-let chute = prompt('Escolha um número entre 1 e 30: ');
+let chute
+let tentativas = 0;
 
-if (chute == numeroSecreto)
+while (chute != numeroSecreto & tentativas < 3)
 {
-    console.log(`Parabéns! Você acertou o número secreto: ${numeroSecreto}!`);
-}
-else
-{
-    if (chute < numeroSecreto)
+    chute = prompt('Escolha um número entre 1 e 30: ');
+    tentativas++;
+
+    if (chute == numeroSecreto)
     {
-        alert(`O número secreto é maior que ${chute}`);
+        console.log(`Parabéns! Você acertou o número secreto: ${numeroSecreto}!`);
     }
     else
     {
-        alert(`O número secreto é menor que ${chute}`);
+        if (chute < numeroSecreto)
+        {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        else
+        {
+            alert(`O número secreto é menor que ${chute}`);
+        }
     }
 }
-
+if (tentativas > 3)
+{
+    alert('Você excedeu o número de tentativas permitido!');
+}
