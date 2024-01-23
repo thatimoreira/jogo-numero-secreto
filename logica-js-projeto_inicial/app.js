aa383a216f8f277f4f4a13e248c7860d970e8aa1
thatimoreira <thatimoreira.dev@gmail.com>
2024-01-23 14:07:56 -0300

    Implementação de lógica para recebimento de entrada do usuário, exibição das mensagens do jogo e verificação de acerto ou não do número secreto, além de comparar o 'palpite' do usuário com o número secreto indicando se este é maior ou menor que o número digitado pelo jogador

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,30 +1,77 @@
-alert('Boas vindas ao Jogo do Número Secreto!');
-
-let numMinimo = 1;
-let numMaximo = 100;
-let numeroSecreto = parseInt(Math.random() * numMaximo) + numMinimo;
-let chute;
+let numeroSecreto = 7; //gerarAleatorio();
 let tentativas = 1;
 
-while (chute != numeroSecreto )
+function exibirMensagemTela(tag, texto)
 {
-    chute = prompt(`Escolha um número entre ${numMinimo} e ${numMaximo}: `);
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial()
+{
+    exibirMensagemTela('h1', 'Jogo do Número Secreto');
+    exibirMensagemTela('p', 'Escolha um número entre 1 e 100:');
+}
+
+function verificarChute()
+{
+    let chute = document.querySelector('input').value;
 
     if (chute == numeroSecreto)
     {
-        let palavraTentativa = tentativas > 1 ? 'tentativas.' : 'tentativa.'
-        alert(`Parabéns!\nVocê acertou o número secreto ${numeroSecreto}!\n com ${tentativas} ${palavraTentativa}`);
+        let mensagem = `Você descobriu o número secreto ${numeroSecreto}`;
+        exibirMensagemTela('h1', 'Parabéns!');
+        exibirMensagemTela('p', mensagem);
     }
     else
     {
         if (chute < numeroSecreto)
         {
-            alert(`O número secreto é maior que ${chute}`);
+            let mensagem = `O número secreto é maior que ${chute}`;
+            exibirMensagemTela('p', mensagem);
         }
         else
         {
-            alert(`O número secreto é menor que ${chute}`);
+            let mensagem = `O número secreto é menor que ${chute}`;
+            exibirMensagemTela('p', mensagem);
+        }
+    }
+}
+
+exibirMensagemInicial();
+
+
+
+
+
+
+/*
+let numMinimo = 1;
+let numMaximo = 100;
+let numeroSecreto = 7; //parseInt(Math.random() * numMaximo) + numMinimo;
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto )
+{
+    //chute = prompt(`Escolha um número entre ${numMinimo} e ${numMaximo}: `);
+
+    if (chute == numeroSecreto)
+    {
+        let palavraTentativa = tentativas > 1 ? 'tentativas.' : 'tentativa.'
+        //alert(`Parabéns!\nVocê acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+    }
+    else
+    {
+        if (chute < numeroSecreto)
+        {
+            //alert(`O número secreto é maior que ${chute}`);
+        }
+        else
+        {
+            //alert(`O número secreto é menor que ${chute}`);
         }
         tentativas++;
     }
 }
+*/
