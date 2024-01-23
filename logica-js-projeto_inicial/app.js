74c74dc5bd3295ae3e85946f3bbc7e59c6aef095
thatimoreira <thatimoreira.dev@gmail.com>
2024-01-23 15:26:47 -0300

    Correção de bug na linha 58, pois faltava o comando 'document' antes de 'getElementById' para que o botão 'Novo jogo' funcionasse corretamente. Deletei lógica antiga que estava comentada ao final do código

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -13,6 +13,8 @@ function exibirMensagemInicial()
     exibirMensagemTela('p', 'Escolha um número entre 1 e 100:');
 }
 
+exibirMensagemInicial();
+
 function verificarChute()
 {
     let chute = document.querySelector('input').value;
@@ -49,47 +51,9 @@ function limparCampo()
 
 function reiniciarJogo()
 {
-    gerarNumeroAleatorio();
-    exibirMensagemInicial();
-    limparCampo();
+    numeroSecreto = gerarNumeroAleatorio();
     tentativas = 1;
-    getElementById('reiniciar').setAttribute('disabled', true);
+    limparCampo();
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
-
-exibirMensagemInicial();
-
-
-
-
-
-
-/*
-let numMinimo = 1;
-let numMaximo = 100;
-let numeroSecreto = 7; //parseInt(Math.random() * numMaximo) + numMinimo;
-let chute;
-let tentativas = 1;
-
-while (chute != numeroSecreto )
-{
-    //chute = prompt(`Escolha um número entre ${numMinimo} e ${numMaximo}: `);
-
-    if (chute == numeroSecreto)
-    {
-        let palavraTentativa = tentativas > 1 ? 'tentativas.' : 'tentativa.'
-        //alert(`Parabéns!\nVocê acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
-    }
-    else
-    {
-        if (chute < numeroSecreto)
-        {
-            //alert(`O número secreto é maior que ${chute}`);
-        }
-        else
-        {
-            //alert(`O número secreto é menor que ${chute}`);
-        }
-        tentativas++;
-    }
-}
-*/
